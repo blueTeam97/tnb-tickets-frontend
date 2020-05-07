83b43918114d0de8fbebc05bb44d1df80e640e15
unknown <carinamaican@yahoo.com>
2020-05-07 23:27:47 +0300

    add validation login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormGroup, FormBuilder } from '@angular/forms';
+import { CustomValidationService} from '../../services/custom-validation.service'
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -6,9 +9,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  registerForm: FormGroup;
+  submitted = false;
 
-  ngOnInit(): void {
+  constructor(
+    private fb: FormBuilder,
+    private customValidator: CustomValidationService
+
+
+  ) { }
+
+  ngOnInit() {
+    this.registerForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.compose([Validators.required, this.customValidator.patternValidator()])]
+    }
+    );
+  }
+
+  get registerFormControl() {
+    return this.registerForm.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    if (this.registerForm.valid) {
+      alert('Form Submitted succesfully!!!\n Check the values in browser console.');
+      console.table(this.registerForm.value);
+    }
   }
 
 }

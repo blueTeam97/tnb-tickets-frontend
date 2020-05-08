9bb074ea72206057f83198295a4280dfa0c88ddd
Denis Visan <denis.visan@inomedica.ro>
2020-05-08 14:48:52 +0300

    add logged-in-module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 
-
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
-    path: 'admin',
-    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
-  },
-  {
-    path: 'user',
-    loadChildren: () => import('./user/user.module').then(m => m.UserModule)
+    path: '',
+    loadChildren: () => import('./logged-in/logged-in.module').then(m => m.LoggedInModule)
   }
 ];
 
---
+++ src/app/logged-in/logged-in-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoggedInComponent } from './logged-in.component';
+
+const routes: Routes = [
+  {
+    path: '', component: LoggedInComponent,
+    children: [
+      {
+        path: 'admin',
+        loadChildren: () => import('../admin/admin.module').then(m => m.AdminModule)
+      },
+      {
+        path: 'user',
+        loadChildren: () => import('../user/user.module').then(m => m.UserModule)
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class LoggedInRoutingModule { }

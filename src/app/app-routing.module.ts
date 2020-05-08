8518c27843a5e2d0876142f4d2e724941dceed7d
cumpatacostin <andreicostinc@gmail.com>
2020-05-08 11:56:21 +0300

    create user module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,12 @@ import { LoginComponent } from './pages/login/login.component';
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {
-    path: '',
+    path: 'admin',
     loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+  },
+  {
+    path: 'user',
+    loadChildren: () => import('./user/user.module').then(m => m.UserModule)
   }
 ];
 

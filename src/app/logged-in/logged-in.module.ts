16f15b671f1b2909daf239521ca5bbcbfe1fb7b7
Denis Visan <denis.visan@inomedica.ro>
2020-05-12 13:23:53 +0300

    switch from navBar to ngNavBar

--- src/app/logged-in/logged-in.module.ts
+++ src/app/logged-in/logged-in.module.ts
@@ -5,11 +5,14 @@ import { LoggedInRoutingModule } from './logged-in-routing.module';
 import { LoggedInComponent } from './logged-in.component';
 import { NavComponent } from './nav/nav/nav.component';
 
+import { NgbNavModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [LoggedInComponent, NavComponent],
   imports: [
     CommonModule,
+    NgbNavModule,
+    NgbDropdownModule,
     LoggedInRoutingModule
   ]
 })

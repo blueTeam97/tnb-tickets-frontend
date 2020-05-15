5ec215260504561f735ae2de91c344bff84c78cf
Vanuta Mihai <mihai.vanuta@gmail.com>
2020-05-15 10:32:17 +0300

    add logout and user history fix

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Validators, FormGroup, FormBuilder } from '@angular/forms';
-import { CustomValidationService } from '../../services/custom-validation.service'
+import { CustomValidationService } from '../../services/custom-validation.service';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { TokenStorageService } from 'src/app/services/auth/token-storage.service';
 import { AuthLoginInfo } from 'src/app/models/auth/login-info';
@@ -9,10 +9,9 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
   registerForm: FormGroup;
   submitted = false;
   private loginInfo: AuthLoginInfo;
@@ -23,16 +22,22 @@ export class LoginComponent implements OnInit {
     private customValidator: CustomValidationService,
     private authService: AuthService,
     private tokenStorage: TokenStorageService,
-    private router: Router) { }
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.checkLoggedIn();
     this.registerForm = this.fb.group({
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.compose([Validators.required, this.customValidator.patternValidator("password")])],
-      error: ['']
-    }
-    );
+      password: [
+        '',
+        Validators.compose([
+          Validators.required,
+          this.customValidator.patternValidator('password'),
+        ]),
+      ],
+      error: [''],
+    });
   }
 
   get registerFormControl() {
@@ -47,41 +52,38 @@ export class LoginComponent implements OnInit {
         this.registerForm.get('email').value,
         this.registerForm.get('password').value
       );
-      console.log(this.loginInfo)
+      console.log(this.loginInfo);
       this.attemptAuth(this.loginInfo);
     }
   }
 
   attemptAuth(loginInfo: AuthLoginInfo) {
     this.authService.attemptAuth(this.loginInfo).subscribe(
-      data => {
+      (data) => {
         this.tokenStorage.saveToken(data.accessToken);
         this.tokenStorage.saveUsername(data.username);
         this.tokenStorage.saveAuthorities(data.authorities);
 
         this.checkLoggedIn();
       },
-      error => {
+      (error) => {
         this.error = error.error;
         console.log(this.registerForm.get('error').value);
-      });
+      }
+    );
   }
 
   checkLoggedIn() {
     let roles = [];
     if (this.tokenStorage.getToken()) {
       roles = this.tokenStorage.getAuthorities();
-      roles.every(role => {
+      roles.every((role) => {
         if (role === 'user') {
-          this.router.navigate(['user'])
-        }
-        else if (role === 'admin') {
-          this.router.navigate(['admin'])
+          this.router.navigate(['user']);
+        } else if (role === 'admin') {
+          this.router.navigate(['admin']);
         }
       });
     }
   }
-
 }
-
-

10c8fa7be9a4fe8c4c057b76bb1564574d820220
Vanuta Mihai <mihai.vanuta@gmail.com>
2020-05-15 17:14:33 +0300

    PlaysFilterPipe import fixed

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -7,8 +7,7 @@ import { HistoryComponent } from './pages/history/history.component';
 import { PlaysComponent } from './pages/plays/plays.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { NgbTypeaheadModule } from '@ng-bootstrap/ng-bootstrap'
-import { PlaysFilterPipe } from 'src/pipes/plays-filter.pipe';
-//import { PlaysFilterPipe } from '../../pipes/plays-filter.pipe';
+import { PlaysFilterPipe } from '../../pipes/plays-filter.pipe';
 
 @NgModule({
   declarations: [UserComponent, HistoryComponent, PlaysComponent,PlaysFilterPipe],

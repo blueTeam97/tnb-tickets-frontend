a2aeba74ae794b5b0146daec8c6e55b2a5540f46
unknown <carinamaican@yahoo.com>
2020-05-26 11:53:17 +0300

    modify table Plays/Tickets page
        pick ea1a6c1 modify table Plays/Tickets page

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -45,7 +45,7 @@ export class AdminComponent implements OnInit {
   }
 
   deletePlay(id: number) {
-    this.confirmationDialogService.confirm('Please confirm..', 'Are you sure to delete this play?')
+    this.confirmationDialogService.confirm(' ', 'Are you sure you want to delete this play?')
         .then((confirmed) => {
           if(confirmed) {
             this.dataService.deletePlayRequest(id).subscribe(() => {
